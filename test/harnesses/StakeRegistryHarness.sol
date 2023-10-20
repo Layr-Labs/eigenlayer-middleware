84b5a5df09b90cd1a8958441f7b637ff0595c2df
steven <steven.nevins@eigenlabs.org>
2023-10-20 12:32:01 -0400

    add view methods for compatability with core temporarily

--- test/harnesses/StakeRegistryHarness.sol
+++ test/harnesses/StakeRegistryHarness.sol
@@ -31,6 +31,11 @@ contract StakeRegistryHarness is StakeRegistry {
         return _weightOfOperatorForQuorum[quorumNumber][operator];
     }
 
+    /// TODO remove when core gets updated
+    function weightOfOperatorForQuorumView(uint8 quorumNumber, address operator) public override view returns(uint96) {
+        return _weightOfOperatorForQuorum[quorumNumber][operator];
+    }
+
     // mocked function so we can set this arbitrarily without having to mock other elements
     function setOperatorWeight(uint8 quorumNumber, address operator, uint96 weight) external {
         _weightOfOperatorForQuorum[quorumNumber][operator] = weight;
