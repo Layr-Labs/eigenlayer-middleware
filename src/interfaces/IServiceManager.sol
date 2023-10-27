d367d8b1b9f175ce470646d455c2b449e95d219f
steven <steven.nevins@eigenlabs.org>
2023-10-27 09:10:01 -0400

    chore: remove unneeded interface methods

--- src/interfaces/IServiceManager.sol
+++ src/interfaces/IServiceManager.sol
@@ -21,19 +21,6 @@ interface IServiceManager {
     /// @dev this function should contain slashing logic, to make sure operators are not needlessly being slashed
     function freezeOperator(address operator) external;
 
-    /// @notice proxy call to the slasher, recording an initial stake update (on operator registration)
-    function recordFirstStakeUpdate(address operator, uint32 serveUntilBlock) external;
-
-    /// @notice proxy call to the slasher, recording a stake update
-    function recordStakeUpdate(address operator, uint32 updateBlock, uint32 serveUntilBlock, uint256 prevElement) external;
-
-    /// @notice proxy call to the slasher, recording a final stake update (on operator deregistration)
-    function recordLastStakeUpdateAndRevokeSlashingAbility(address operator, uint32 serveUntilBlock) external;
-
-    /// @notice Returns the latest block until which operators must serve (could be in the past or future).
-    /// @dev this should be called and the response passed to the recordStakeUpdate functionss' serveUntilBlock parameter
-    function latestServeUntilBlock() external view returns (uint32);
-
     /// @notice required since the registry contract will call this function to permission its upgrades to be done by the same owner as the service manager
     function owner() external view returns (address);
 }
