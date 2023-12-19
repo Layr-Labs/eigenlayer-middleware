599e018256b24bb1cc46a80cb19a109dfce68baf
8sunyuan <michaelsun97@gmail.com>
2023-12-19 01:34:56 -0500

    fix: undo change

--- src/StakeRegistry.sol
+++ src/StakeRegistry.sol
@@ -185,7 +185,7 @@ contract StakeRegistry is StakeRegistryStorage {
             _recordTotalStakeUpdate(quorumNumber, stakeDelta);
         }
 
-        return uint192(quorumsToRemove);
+        return quorumsToRemove;
     }
 
     /// @notice Initialize a new quorum and push its first history update
