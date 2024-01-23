aaeb54ca42b1c655e6adcb16644690f27c123809
8sunyuan <michaelsun97@gmail.com>
2024-01-23 11:19:34 -0500

    fix: setup certora script

---
+++ certora/scripts/verifyRegistryCoordinator.sh
@@ -0,0 +1,22 @@
+if [[ "$2" ]]
+then
+    RULE="--rule $2"
+fi
+
+solc-select use 0.8.12
+
+certoraRun certora/harnesses/RegistryCoordinatorHarness.sol \
+    lib/openzeppelin-contracts/contracts/mocks/ERC1271WalletMock.sol \
+    src/StakeRegistry.sol src/BLSApkRegistry.sol src/IndexRegistry.sol \
+    lib/eigenlayer-contracts/src/contracts/core/Slasher.sol \
+    --verify RegistryCoordinatorHarness:certora/specs/RegistryCoordinator.spec \
+    --optimistic_loop \
+    --optimistic_hashing \
+    --prover_args '-optimisticFallback true -recursionEntryLimit 2 ' \
+    $RULE \
+    --loop_iter 2 \
+    # --packages @openzeppelin=lib/eigenlayer-contracts/lib/openzeppelin-contracts @openzeppelin-upgrades=lib/eigenlayer-contracts/lib/openzeppelin-contracts-upgradeable eigenlayer-contracts=lib/eigenlayer-contracts \
+    --packages @openzeppelin=lib/openzeppelin-contracts/ @openzeppelin-upgrades=lib/openzeppelin-contracts-upgradeable/ eigenlayer-contracts=lib/eigenlayer-contracts/ \
+    --msg "RegistryCoordinator $1 $2" \
+
+# TODO: import a ServiceManager contract
