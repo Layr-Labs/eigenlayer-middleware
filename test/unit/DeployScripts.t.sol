4595e0420e971b91f8fb9686a3d17ef52e3567cc
steven <steven.nevins@eigenlabs.org>
2024-02-22 17:23:47 -0500

    test(wip): testing setting up tests of individual deploy functions

--- test/unit/DeployScripts.t.sol
+++ test/unit/DeployScripts.t.sol
@@ -4,8 +4,19 @@ pragma solidity =0.8.12;
 import {Test, console} from "forge-std/Test.sol";
 import {DeployUtilsLocal} from "../../script/AVSContractsDeploy.s.sol";
 import {TimeMachine} from "../integration/TimeMachine.t.sol";
+import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+import "eigenlayer-contracts/src/contracts/permissions/PauserRegistry.sol";
+import "eigenlayer-contracts/src/contracts/core/StrategyManager.sol";
+import "eigenlayer-contracts/src/contracts/core/Slasher.sol";
 
 contract LocalScriptTest is DeployUtilsLocal, Test, TimeMachine {
+    function setUp() public {
+        unpauser = makeAddr("unpauser");
+        address pauser1 = makeAddr("pauser 2");
+        address pauser2 = makeAddr("pauser 2");
+        pausers.push(pauser1);
+        pausers.push(pauser2);
+    }
     function test_AddressFrom() public {
         uint256 nonce = vm.getNonce(address(this));
         address predicted = _addressFrom(address(this), nonce);
@@ -30,13 +41,24 @@ contract LocalScriptTest is DeployUtilsLocal, Test, TimeMachine {
     }
 
     function test_CoreLocal() public {
-        createSnapshot();
+        uint256 initialState = createSnapshot();
+        proxyAdmin = ProxyAdmin(_deployProxyAdmin());
         /// TODO: deploy each separately
+        /// TODO: Need to precompute addresses since there is no validation in the constructors
+        pauserRegistry = PauserRegistry(_deployPauserRegistry(pausers, unpauser));
 
+        console.log(vm.getNonce(address(this)));
+        strategyManager = StrategyManager(_deployStrategyManager(0));
+        console.log(vm.getNonce(address(this)));
+
+        slasher = Slasher(_deploySlasher());
+        console.log(vm.getNonce(address(this)));
         /// TODO: verify state
         warpToLast();
 
-        _deployCore();
+        vm.revertTo(initialState);
+
+        // _deployCore();
 
         /// TODO: Verify deployment state
     }
