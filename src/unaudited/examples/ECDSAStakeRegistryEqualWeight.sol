1c44de627a57b2df148998d5a8849221f621d894
steven <steven.nevins.js@gmail.com>
2024-03-04 09:52:23 -0500

    chore: cleanup

--- src/unaudited/examples/ECDSAStakeRegistryEqualWeight.sol
+++ src/unaudited/examples/ECDSAStakeRegistryEqualWeight.sol
@@ -6,19 +6,29 @@ import {ECDSAStakeRegistryPermissioned} from "./ECDSAStakeRegistryPermissioned.s
 import {IDelegationManager} from "eigenlayer-contracts/src/contracts/interfaces/IDelegationManager.sol";
 import {CheckpointsUpgradeable} from "@openzeppelin-upgrades/contracts/utils/CheckpointsUpgradeable.sol";
 
+/// @title ECDSA Stake Registry with Equal Weight
+/// @notice A contract to manage operator stakes with equal weighting for operators
+/// @dev Inherits from ECDSAStakeRegistryPermissioned to leverage permissioned stake registry functionality.
 contract ECDSAStakeRegistryEqualWeight is ECDSAStakeRegistryPermissioned {
     using CheckpointsUpgradeable for CheckpointsUpgradeable.History;
+
+    /// @notice Initializes the contract with a specified delegation manager.
+    /// @dev Passes the delegation manager to the parent constructor.
+    /// @param _delegationManager The address of the delegation manager contract.
     constructor(
         IDelegationManager _delegationManager
     ) ECDSAStakeRegistryPermissioned(_delegationManager) {
         // _disableInitializers();
     }
 
+    /// @notice Updates the weight of an operator in the stake registry.
+    /// @dev Overrides the _updateOperatorWeight function from the parent class to implement equal weighting.
+    ///      Emits an OperatorWeightUpdated event upon successful update.
+    /// @param _operator The address of the operator whose weight is being updated.
     function _updateOperatorWeight(address _operator) internal override {
         uint256 oldWeight;
         uint256 newWeight;
         int256 delta;
-        /// TODO: Need to fix this logic
         if (_operatorRegistered[_operator]) {
             (oldWeight, ) = _operatorWeightHistory[_operator].push(1);
             delta = int256(1) - int(oldWeight); // handles if they were already registered
@@ -26,7 +36,7 @@ contract ECDSAStakeRegistryEqualWeight is ECDSAStakeRegistryPermissioned {
             (oldWeight, ) = _operatorWeightHistory[_operator].push(0);
             delta = int256(0) - int(oldWeight);
         }
-        _updateTotalWeight(delta);
         emit OperatorWeightUpdated(_operator, oldWeight, newWeight);
+        _updateTotalWeight(delta);
     }
 }
--- src/unaudited/examples/ECDSAStakeRegistryPermissioned.sol
+++ src/unaudited/examples/ECDSAStakeRegistryPermissioned.sol
@@ -5,27 +5,39 @@ import {ISignatureUtils} from "eigenlayer-contracts/src/contracts/interfaces/ISi
 import {ECDSAStakeRegistry} from "../ECDSAStakeRegistry.sol";
 import {IDelegationManager} from "eigenlayer-contracts/src/contracts/interfaces/IDelegationManager.sol";
 
+/// @title ECDSA Stake Registry with an Operator Allowlist
+/// @notice This contract extends ECDSAStakeRegistry by adding functionality to allowlist and remove operators
 contract ECDSAStakeRegistryPermissioned is ECDSAStakeRegistry {
     mapping(address => bool) public allowlistedOperators;
 
+    /// @dev Custom error to signal that an operator is not allowlisted.
     error OperatorNotAllowlisted();
+
     constructor(IDelegationManager _delegationManager) ECDSAStakeRegistry(_delegationManager) {
         // _disableInitializers();
     }
 
+    /// @notice Adds an operator to the allowlisted operator set
+    /// @dev An allowlisted operator isn't a part of the operator set. They must subsequently register themselves
+    /// @param _operator The address of the operator to be allowlisted
     function permitOperator(address _operator) external onlyOwner {
         allowlistedOperators[_operator] = true;
     }
 
+    /// @notice Revokes an operator's permission and deregisters them
+    /// @param _operator The address of the operator to remove from the allowlist and deregistered.
     function revokeOperator(address _operator) external onlyOwner {
         delete allowlistedOperators[_operator];
         _deregisterOperator(_operator);
     }
 
+    /// @notice Directly deregisters an operator without removing from the allowlist
+    /// @param _operator The address of the operator to deregister
     function ejectOperator(address _operator) external onlyOwner {
         _deregisterOperator(_operator);
     }
 
+    /// @inheritdoc ECDSAStakeRegistry
     function _registerOperatorWithSig(
         address _operator,
         ISignatureUtils.SignatureWithSaltAndExpiry memory _operatorSignature
