a513621c39f7d2edcb3c8409e30d23241ab22eeb
steven <steven.nevins.js@gmail.com>
2024-03-11 16:04:50 -0400

    refactor: logic into internal functions and add events

--- src/unaudited/examples/ECDSAStakeRegistryPermissioned.sol
+++ src/unaudited/examples/ECDSAStakeRegistryPermissioned.sol
@@ -10,8 +10,20 @@ import {IDelegationManager} from "eigenlayer-contracts/src/contracts/interfaces/
 contract ECDSAStakeRegistryPermissioned is ECDSAStakeRegistry {
     mapping(address => bool) public allowlistedOperators;
 
+    /// @dev Emits when an operator is added to the allowlist.
+    event OperatorPermitted(address indexed operator);
+
+    /// @dev Emits when an operator is removed from the allowlist.
+    event OperatorRevoked(address indexed operator);
+
+    /// @dev Emits when an operator is removed from the active operator set.
+    event OperatorEjected(address indexed operator);
+
     /// @dev Custom error to signal that an operator is not allowlisted.
     error OperatorNotAllowlisted();
+
+    /// @dev Custom error to signal that an operator is already allowlisted.
+    error OperatorAlreadyAllowlisted();
 
     constructor(IDelegationManager _delegationManager) ECDSAStakeRegistry(_delegationManager) {
         // _disableInitializers();
@@ -21,22 +33,53 @@ contract ECDSAStakeRegistryPermissioned is ECDSAStakeRegistry {
     /// @dev An allowlisted operator isn't a part of the operator set. They must subsequently register themselves
     /// @param _operator The address of the operator to be allowlisted
     function permitOperator(address _operator) external onlyOwner {
-        allowlistedOperators[_operator] = true;
+        _permitOperator(_operator);
     }
 
     /// @notice Revokes an operator's permission and deregisters them
+    /// @dev Emits the OperatorRevoked event if the operator was previously allowlisted.
     /// @param _operator The address of the operator to remove from the allowlist and deregistered.
     function revokeOperator(address _operator) external onlyOwner {
-        delete allowlistedOperators[_operator];
-        if (_operatorRegistered[_operator]){
-            _deregisterOperator(_operator);
-        }
+        _revokeOperator(_operator);
     }
 
     /// @notice Directly deregisters an operator without removing from the allowlist
+    /// @dev Does not emit an event because it does not modify the allowlist.
     /// @param _operator The address of the operator to deregister
     function ejectOperator(address _operator) external onlyOwner {
+        _ejectOperator(_operator);
+    }
+
+    /// @dev Deregisters and operator from the active operator set
+    /// @param _operator The address of the operator to remove.
+    function _ejectOperator(address _operator) internal {
         _deregisterOperator(_operator);
+        emit OperatorEjected(_operator);
+    }
+
+    /// @dev Adds an operator to the allowlisted operator set
+    /// Doesn't register the operator into the operator set
+    /// @param _operator The address of the operator to allowlist.
+    function _permitOperator(address _operator) internal {
+        if (allowlistedOperators[_operator]){
+            revert OperatorAlreadyAllowlisted();
+        }
+        allowlistedOperators[_operator] = true;
+        emit OperatorPermitted(_operator);
+
+    }
+
+    /// @dev Removes an operator from the allowlist.
+    /// If the operator is registered, also deregisters the operator.
+    /// @param _operator The address of the operator to be revoked.
+    function _revokeOperator(address _operator) internal {
+        if (!allowlistedOperators[_operator]) revert OperatorNotAllowlisted();
+        delete allowlistedOperators[_operator];
+        emit OperatorRevoked(_operator);
+        if (_operatorRegistered[_operator]){
+            _ejectOperator(_operator);
+        }
+
     }
 
     /// @inheritdoc ECDSAStakeRegistry
@@ -48,3 +91,4 @@ contract ECDSAStakeRegistryPermissioned is ECDSAStakeRegistry {
         super._registerOperatorWithSig(_operator, _operatorSignature);
     }
 }
+
