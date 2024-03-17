fee665f5185c4787462a413371160b9a77dd69db
yonikesel1 <49488355+yonikesel1@users.noreply.github.com>
2024-03-17 14:43:51 +0200

    fix: goerli deploy script import (#22)

--- script/deploy/goerli/Goerli_DeployEOMiddlewareContracts.s.sol
+++ script/deploy/goerli/Goerli_DeployEOMiddlewareContracts.s.sol
@@ -3,9 +3,9 @@ pragma solidity =0.8.12;
 
 import "forge-std/Script.sol";
 import "forge-std/StdJson.sol";
-import "eigenlayer-contracts/script/utils/ExistingDeploymentParser.sol";
 
 import {Utils} from "../../utils/Utils.s.sol";
+import {ExistingDeploymentParser} from "../../utils/ExistingDeploymentParser.sol";
 
 // OpenZeppelin
 import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
@@ -20,7 +20,8 @@ import {PauserRegistry} from "eigenlayer-contracts/src/contracts/permissions/Pau
 import {
     EORegistryCoordinator,
     IEORegistryCoordinator,
-    IEOStakeRegistry
+    IEOStakeRegistry,
+    IPauserRegistry
     } from "../../../src/EORegistryCoordinator.sol";
 import {EOBLSApkRegistry} from "../../../src/EOBLSApkRegistry.sol";
 import {EOIndexRegistry} from "../../../src/EOIndexRegistry.sol";
