114243775f3abf595013a3722ffc9bbd574e4c98
steven <12021290+stevennevins@users.noreply.github.com>
2024-05-24 10:51:56 -0400

    fix: storage gap (#261)

--- src/unaudited/ECDSAStakeRegistryStorage.sol
+++ src/unaudited/ECDSAStakeRegistryStorage.sol
@@ -54,5 +54,5 @@ abstract contract ECDSAStakeRegistryStorage is
     // slither-disable-next-line shadowing-state
     /// @dev Reserves storage slots for future upgrades
     // solhint-disable-next-line
-    uint256[39] private __gap;
+    uint256[40] private __gap;
 }
