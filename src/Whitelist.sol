c30681d424b5e353b535c4f46998a26b19ec59be
PatStiles <pdstiles78@gmail.com>
2024-09-17 19:07:14 -0300

    emit events in add and remove within whitelist contract

--- src/Whitelist.sol
+++ src/Whitelist.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.12;
 
 import {OwnableUpgradeable} from "@openzeppelin-upgrades/contracts/access/OwnableUpgradeable.sol";
 
+event AddedToWhiteList(address indexed added);
+event RemovedFromWhiteList(address indexed removed);
 /*
  * Note: The OwnableUpgradeable contract is used to ensure that only the owner of the contract
  * can add or remove addresses from the whitelist. It must be OwnableUpgradeable instead of Ownable
@@ -18,10 +20,12 @@ contract Whitelist is OwnableUpgradeable {
 
     function add(address _address) public onlyOwner {
         whitelist[_address] = true;
+        emit AddedToWhiteList(_address);
     }
 
     function remove(address _address) public onlyOwner {
         whitelist[_address] = false;
+        emit RemovedFromWhiteList(_address);
     }
 
     function isWhitelisted(address _address) public view returns (bool) {
