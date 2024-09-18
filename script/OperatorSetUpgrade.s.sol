98c569211fe5be3dec962faa81596fad6192ab96
steven <steven.nevins.js@gmail.com>
2024-09-18 10:37:26 -0400

    fix: ci build pragra issue

--- script/OperatorSetUpgrade.s.sol
+++ script/OperatorSetUpgrade.s.sol
@@ -1,5 +1,5 @@
-// // SPDX-License-Identifier: BUSL-1.1
-// pragma solidity ^0.8.0;
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.0;
 
 // import {Script, console2} from "forge-std/Script.sol";
 // import {OperatorSetUpgradeLib} from "./utils/UpgradeLib.sol";
