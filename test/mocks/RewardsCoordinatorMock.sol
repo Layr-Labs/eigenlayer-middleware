b0a93b06e768429fdac139d9c97cd632a6150c0b
steven <steven.nevins.js@gmail.com>
2024-10-28 12:14:55 -0400

    fix: intefface changes from dependency bump

--- test/mocks/RewardsCoordinatorMock.sol
+++ test/mocks/RewardsCoordinatorMock.sol
@@ -6,7 +6,6 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IRewardsCoordinator} from "eigenlayer-contracts/src/contracts/interfaces/IRewardsCoordinator.sol";
 
 contract RewardsCoordinatorMock is IRewardsCoordinator {
-    /// @notice The address of the entity that can update the contract with new merkle roots
     function rewardsUpdater() external view returns (address) {}
 
     function CALCULATION_INTERVAL_SECONDS() external view returns (uint32) {}
@@ -37,44 +36,38 @@ contract RewardsCoordinatorMock is IRewardsCoordinator {
 
     function currRewardsCalculationEndTimestamp() external view returns (uint32) {}
 
-    function getRootIndexFromHash(bytes32 rootHash) external view returns (uint32) {}
-
     function getDistributionRootsLength() external view returns (uint256) {}
-
-    function getCurrentClaimableDistributionRoot() external view returns (DistributionRoot memory) {}
 
     function getDistributionRootAtIndex(uint256 index) external view returns (DistributionRoot memory) {}
 
     function getCurrentDistributionRoot() external view returns (DistributionRoot memory) {}
 
-    /// EXTERNAL FUNCTIONS ///
+    function getCurrentClaimableDistributionRoot() external view returns (DistributionRoot memory) {}
 
-    function disableRoot(uint32 rootIndex) external {}
+    function getRootIndexFromHash(bytes32 rootHash) external view returns (uint32) {}
+
+    function domainSeparator() external view returns (bytes32) {}
 
     function createAVSRewardsSubmission(RewardsSubmission[] calldata rewardsSubmissions) external {}
 
     function createRewardsForAllSubmission(RewardsSubmission[] calldata rewardsSubmission) external {}
 
+    function createRewardsForAllEarners(RewardsSubmission[] calldata rewardsSubmissions) external {}
+
     function processClaim(RewardsMerkleClaim calldata claim, address recipient) external {}
 
-    function submitRoot(
-        bytes32 root,
-        uint32 rewardsCalculationEndTimestamp
-    ) external {}
+    function submitRoot(bytes32 root, uint32 rewardsCalculationEndTimestamp) external {}
 
-    function setRewardsUpdater(address _rewardsUpdater) external {}
+    function disableRoot(uint32 rootIndex) external {}
+
+    function setClaimerFor(address claimer) external {}
 
     function setActivationDelay(uint32 _activationDelay) external {}
 
     function setGlobalOperatorCommission(uint16 _globalCommissionBips) external {}
 
-    function setClaimerFor(address claimer) external {}
+    function setRewardsUpdater(address _rewardsUpdater) external {}
 
-    /**
-     * @notice Sets the permissioned `payAllForRangeSubmitter` address which can submit payAllForRange
-     * @dev Only callable by the contract owner
-     * @param _submitter The address of the payAllForRangeSubmitter
-     * @param _newValue The new value for isPayAllForRangeSubmitter
-     */
     function setRewardsForAllSubmitter(address _submitter, bool _newValue) external {}
+
 }
