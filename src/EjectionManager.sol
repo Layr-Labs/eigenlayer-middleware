66d4884a63e56631985c81af7da342ee75fa6246
QUAQ <archk.latham@gmail.com>
2024-11-19 15:20:21 -0600

    fix: for real this time

--- src/EjectionManager.sol
+++ src/EjectionManager.sol
@@ -74,17 +74,30 @@ contract EjectionManager is IEjectionManager, OwnableUpgradeable{
             uint32 ejectedOperators;
 
             bool ratelimitHit;
-            for(uint8 j = 0; j < _operatorIds[i].length; ++j) {
-                uint256 operatorStake = stakeRegistry.getCurrentStake(_operatorIds[i][j], quorumNumber);
+            if(amountEjectable > 0){
+                for(uint8 j = 0; j < _operatorIds[i].length; ++j) {
+                    uint256 operatorStake = stakeRegistry.getCurrentStake(_operatorIds[i][j], quorumNumber);
 
-                //if caller is ejector enforce ratelimit
-                if(
-                    isEjector[msg.sender] &&
-                    quorumEjectionParams[quorumNumber].rateLimitWindow > 0 &&
-                    stakeForEjection + operatorStake > amountEjectable &&
-                    amountEjectable > 0
-                ){
-                    ratelimitHit = true;
+                    //if caller is ejector enforce ratelimit
+                    if(
+                        isEjector[msg.sender] &&
+                        quorumEjectionParams[quorumNumber].rateLimitWindow > 0 &&
+                        stakeForEjection + operatorStake > amountEjectable
+                    ){
+                        ratelimitHit = true;
+
+                        stakeForEjection += operatorStake;
+                        ++ejectedOperators;
+
+                        registryCoordinator.ejectOperator(
+                            registryCoordinator.getOperatorFromId(_operatorIds[i][j]),
+                            abi.encodePacked(quorumNumber)
+                        );
+
+                        emit OperatorEjected(_operatorIds[i][j], quorumNumber);
+
+                        break;
+                    }
 
                     stakeForEjection += operatorStake;
                     ++ejectedOperators;
@@ -93,25 +106,13 @@ contract EjectionManager is IEjectionManager, OwnableUpgradeable{
                         registryCoordinator.getOperatorFromId(_operatorIds[i][j]),
                         abi.encodePacked(quorumNumber)
                     );
-
+                    
                     emit OperatorEjected(_operatorIds[i][j], quorumNumber);
-
-                    break;
                 }
-
-                stakeForEjection += operatorStake;
-                ++ejectedOperators;
-
-                registryCoordinator.ejectOperator(
-                    registryCoordinator.getOperatorFromId(_operatorIds[i][j]),
-                    abi.encodePacked(quorumNumber)
-                );
-                
-                emit OperatorEjected(_operatorIds[i][j], quorumNumber);
             }
 
             //record the stake ejected if ejector and ratelimit enforced
-            if(isEjector[msg.sender]){
+            if(isEjector[msg.sender] && stakeForEjection > 0){
                 stakeEjectedForQuorum[quorumNumber].push(StakeEjection({
                     timestamp: block.timestamp,
                     stakeEjected: stakeForEjection
