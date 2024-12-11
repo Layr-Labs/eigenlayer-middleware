0bcfbdeb7f095b09cfb312e9c99c8416efc00b67
Nadir Akhtar <nadirakhtar123@gmail.com>
2024-12-10 17:42:42 -0800

    docs: match file and library name, and add docstrings

--- script/utils/OperatorSetUpgradeLib.sol
+++ script/utils/OperatorSetUpgradeLib.sol
@@ -10,10 +10,22 @@ import {stdJson} from "forge-std/StdJson.sol";
 
 library OperatorSetUpgradeLib {
     using stdJson for string;
-    Vm private constant vm = Vm(address(uint160(uint256(keccak256("hevm cheat code")))));
+
+    // address(uint160(uint256(keccak256("hevm cheat code")))) == 0x7109709ECfa91a80626fF3989D68f67F5b1DD12D
+    // solhint-disable-next-line const-name-snakecase
+    Vm private constant vm = Vm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+
+    /**
+     * @dev Storage slot with the address of the current implementation.
+     * This is the keccak-256 hash of "eip1967.proxy.implementation" subtracted by 1.
+     */
     bytes32 internal constant IMPLEMENTATION_SLOT =
         0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
 
+    /**
+     * @dev Storage slot with the admin of the contract.
+     * This is the keccak-256 hash of "eip1967.proxy.admin" subtracted by 1.
+     */
     bytes32 internal constant ADMIN_SLOT =
      0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103;
 
@@ -28,7 +40,7 @@ library OperatorSetUpgradeLib {
         admin.upgrade(TransparentUpgradeableProxy(payable(proxy)), implementation);
     }
 
-    function getAdmin(address proxy) internal view returns (address){
+    function getAdmin(address proxy) internal view returns (address) {
         bytes32 value = vm.load(proxy, ADMIN_SLOT);
         return address(uint160(uint256(value)));
     }
