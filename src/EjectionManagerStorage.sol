d4a242334dfffbb5e0b399fe15380cc8be24aca4
clandestine.eth <96172957+0xClandestine@users.noreply.github.com>
2025-01-27 13:52:53 -0500

    refactor: add missing gap

--- src/EjectionManagerStorage.sol
+++ src/EjectionManagerStorage.sol
@@ -27,4 +27,8 @@ abstract contract EjectionManagerStorage is IEjectionManager {
         registryCoordinator = _registryCoordinator;
         stakeRegistry = _stakeRegistry;
     }
+
+    /// @dev This was missing before the slashing release, if your contract 
+    /// was deployed pre-slashing, you should double check your storage layout.
+    uint256[47] private __gap;
 }
