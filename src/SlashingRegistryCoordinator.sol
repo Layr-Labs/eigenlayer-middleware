8e7846ee66082912c4b6297b12d9a836bc3f7064
steven <steven.nevins.js@gmail.com>
2025-03-10 12:54:59 -0400

    chore: forge fmt

--- src/SlashingRegistryCoordinator.sol
+++ src/SlashingRegistryCoordinator.sol
@@ -10,7 +10,7 @@ import {
     OperatorSet,
     IAllocationManagerTypes
 } from "eigenlayer-contracts/src/contracts/interfaces/IAllocationManager.sol";
-import {AllocationManager } from "eigenlayer-contracts/src/contracts/core/AllocationManager.sol";
+import {AllocationManager} from "eigenlayer-contracts/src/contracts/core/AllocationManager.sol";
 
 import {IBLSApkRegistry, IBLSApkRegistryTypes} from "./interfaces/IBLSApkRegistry.sol";
 import {IStakeRegistry, IStakeRegistryTypes} from "./interfaces/IStakeRegistry.sol";
@@ -842,7 +842,10 @@ contract SlashingRegistryCoordinator is
             stakeRegistry.initializeDelegatedStakeQuorum(quorumNumber, minimumStake, strategyParams);
         } else if (stakeType == IStakeRegistryTypes.StakeType.TOTAL_SLASHABLE) {
             // For slashable stake quorums, ensure lookAheadPeriod is less than DEALLOCATION_DELAY
-            require(AllocationManager(address(allocationManager)).DEALLOCATION_DELAY() > lookAheadPeriod, LookAheadPeriodTooLong());
+            require(
+                AllocationManager(address(allocationManager)).DEALLOCATION_DELAY() > lookAheadPeriod,
+                LookAheadPeriodTooLong()
+            );
             stakeRegistry.initializeSlashableStakeQuorum(
                 quorumNumber, minimumStake, lookAheadPeriod, strategyParams
             );
