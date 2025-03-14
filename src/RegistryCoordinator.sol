56f7f78b20802d6a2d67be81eb6f7c6c384127c7
Tetiana Afonina <tetiana@eoracle.io>
2025-03-14 20:40:57 +0000

    feat: add virtual keywords to make possible overriding of some functions

--- src/RegistryCoordinator.sol
+++ src/RegistryCoordinator.sol
@@ -63,7 +63,7 @@ contract RegistryCoordinator is RegistryCoordinatorStorage {
         string memory socket,
         IBLSApkRegistryTypes.PubkeyRegistrationParams memory params,
         SignatureWithSaltAndExpiry memory operatorSignature
-    ) external onlyWhenNotPaused(PAUSED_REGISTER_OPERATOR) {
+    ) external virtual onlyWhenNotPaused(PAUSED_REGISTER_OPERATOR) {
         require(!isM2QuorumRegistrationDisabled, M2QuorumRegistrationIsDisabled());
         require(
             quorumNumbers.orderedBytesArrayToBitmap().isSubsetOf(m2QuorumBitmap()),
@@ -97,7 +97,7 @@ contract RegistryCoordinator is RegistryCoordinatorStorage {
         OperatorKickParam[] memory operatorKickParams,
         SignatureWithSaltAndExpiry memory churnApproverSignature,
         SignatureWithSaltAndExpiry memory operatorSignature
-    ) external onlyWhenNotPaused(PAUSED_REGISTER_OPERATOR) {
+    ) external virtual onlyWhenNotPaused(PAUSED_REGISTER_OPERATOR) {
         require(!isM2QuorumRegistrationDisabled, M2QuorumRegistrationIsDisabled());
         require(
             quorumNumbers.orderedBytesArrayToBitmap().isSubsetOf(m2QuorumBitmap()),
