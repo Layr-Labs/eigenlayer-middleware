c0ad6894fa1380627f3781e769f16b1efdc11507
steven <steven.nevins.js@gmail.com>
2025-03-17 16:43:28 -0400

    test: add mock

--- test/mocks/ECDSAStakeRegistryMock.sol
+++ test/mocks/ECDSAStakeRegistryMock.sol
@@ -2,13 +2,49 @@
 pragma solidity ^0.8.27;
 
 import "../../src/unaudited/ECDSAStakeRegistry.sol";
+import {CheckpointsUpgradeable} from
+    "@openzeppelin-upgrades/contracts/utils/CheckpointsUpgradeable.sol";
 
 /**
  * @title Mock for ECDSAStakeRegistry
  * @dev This contract is a mock implementation of the ECDSAStakeRegistry for testing purposes.
  */
 contract ECDSAStakeRegistryMock is ECDSAStakeRegistry {
+    using CheckpointsUpgradeable for CheckpointsUpgradeable.History;
+
     constructor(
         IDelegationManager _delegationManager
     ) ECDSAStakeRegistry(_delegationManager) {}
+
+    /**
+     * @notice Sets the operator weight at a specific block for testing
+     * @param operator The operator address
+     * @param blockNumber The block number
+     * @param weight The weight to set
+     */
+    function setOperatorWeightAtBlock(
+        address operator,
+        uint32 blockNumber,
+        uint256 weight
+    ) external {
+        _operatorWeightHistory[operator].push(weight);
+    }
+
+    /**
+     * @notice Sets the total weight at a specific block for testing
+     * @param blockNumber The block number
+     * @param weight The weight to set
+     */
+    function setTotalWeightAtBlock(uint32 blockNumber, uint256 weight) external {
+        _totalWeightHistory.push(weight);
+    }
+
+    /**
+     * @notice Sets the threshold weight at a specific block for testing
+     * @param blockNumber The block number
+     * @param weight The weight to set
+     */
+    function setThresholdWeightAtBlock(uint32 blockNumber, uint256 weight) external {
+        _thresholdWeightHistory.push(weight);
+    }
 }
