488b54ab537324c556f2b69e5d0d056cf920796b
Yash Patil <40046473+ypatil12@users.noreply.github.com>
2025-04-30 23:48:26 -0400

    chore: fix test

--- test/unit/SlashingRegistryCoordinatorUnit.t.sol
+++ test/unit/SlashingRegistryCoordinatorUnit.t.sol
@@ -1938,7 +1938,6 @@ contract SlashingRegistryCoordinator_RegisterWithChurn is
     }
 
     /// @dev Asserts that an operator cannot be churned out if it is not registered for the quorum
-    /// @dev We register the operator to kick in quorum 2 so that it is still registered after deregistration
     function test_registerOperatorWithChurn_revert_notRegisteredForQuorum() public {
         _setOperatorWeight(testOperator.key.addr, registeringStake);
         _setOperatorWeight(operatorToKick.key.addr, operatorToKickStake);
@@ -1949,6 +1948,12 @@ contract SlashingRegistryCoordinator_RegisterWithChurn is
         strategyParams[0] =
             IStakeRegistryTypes.StrategyParams({strategy: mockStrategy, multiplier: 1 ether});
 
+        operatorSetParams = ISlashingRegistryCoordinatorTypes.OperatorSetParam({
+            maxOperatorCount: 1,
+            kickBIPsOfOperatorStake: 5000,
+            kickBIPsOfTotalStake: 5000
+        });
+
         vm.startPrank(proxyAdminOwner);
         slashingRegistryCoordinator.createTotalDelegatedStakeQuorum(
             operatorSetParams,
@@ -1957,30 +1962,16 @@ contract SlashingRegistryCoordinator_RegisterWithChurn is
         );
         vm.stopPrank();
 
-        // Register the operatorToKick in the new quorum
-        uint32[] memory operatorSetIds = new uint32[](1);
-        operatorSetIds[0] = 2;
-        registerOperatorInSlashingRegistryCoordinator(operatorToKick, "socket:8545", operatorSetIds);
+        // Register extra operator in the new quorum
+        registerOperatorInSlashingRegistryCoordinator(extraOperator1, "socket:8545", uint32(2));
 
-        // Deregister the operatorToKick from quorum 1
-        IAllocationManagerTypes.DeregisterParams memory deregisterParams = IAllocationManagerTypes
-            .DeregisterParams({
-            operator: operatorToKick.key.addr,
-            avs: address(serviceManager),
-            operatorSetIds: operatorSetIds
-        });
 
-        vm.prank(operatorToKick.key.addr);
-        IAllocationManager(coreDeployment.allocationManager).deregisterFromOperatorSets(
-            deregisterParams
-        );
-
-        // Try to churn the operator
+        // Setup churn data
         ISlashingRegistryCoordinatorTypes.OperatorKickParam[] memory operatorKickParams =
             new ISlashingRegistryCoordinatorTypes.OperatorKickParam[](quorumNumbers.length);
         operatorKickParams[0] = ISlashingRegistryCoordinatorTypes.OperatorKickParam({
             operator: operatorToKick.key.addr,
-            quorumNumber: uint8(quorumNumbers[0])
+            quorumNumber: uint8(2) // 3rd quorum
         });
 
         ISignatureUtilsMixinTypes.SignatureWithSaltAndExpiry memory churnApproverSignature =
@@ -1988,7 +1979,7 @@ contract SlashingRegistryCoordinator_RegisterWithChurn is
             testOperator.key.addr,
             testOperatorId,
             operatorKickParams,
-            bytes32(uint256(3)), // Different salt from previous tests
+            bytes32(uint256(4)),
             defaultExpiry
         );
 
@@ -1998,7 +1989,7 @@ contract SlashingRegistryCoordinator_RegisterWithChurn is
         IAllocationManagerTypes.RegisterParams memory registerParams = IAllocationManagerTypes
             .RegisterParams({
             avs: address(serviceManager),
-            operatorSetIds: new uint32[](quorumNumbers.length),
+            operatorSetIds: new uint32[](1),
             data: abi.encode(
                 ISlashingRegistryCoordinatorTypes.RegistrationType.CHURN,
                 "socket:8545",
@@ -2008,9 +1999,7 @@ contract SlashingRegistryCoordinator_RegisterWithChurn is
             )
         });
 
-        for (uint256 i = 0; i < quorumNumbers.length; i++) {
-            registerParams.operatorSetIds[i] = uint8(quorumNumbers[i]);
-        }
+        registerParams.operatorSetIds[0] = uint32(2);
 
         vm.prank(testOperator.key.addr);
         vm.expectRevert(abi.encodeWithSignature("OperatorNotRegisteredForQuorum()"));
