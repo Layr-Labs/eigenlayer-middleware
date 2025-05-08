a84ed4c7a1f3e08dee25e3b23b71d06ddabf3940
Yash Patil <40046473+ypatil12@users.noreply.github.com>
2025-05-08 15:54:53 -0400

    feat: add table calculator interfaces

---
+++ src/interfaces/multichain/IOperatorTableCalculator.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import {OperatorSet} from "eigenlayer-contracts/src/contracts/libraries/OperatorSetLib.sol";
+import {IOperatorWeightCalculator} from "./IOperatorWeightCalculator.sol";
+
+/// @notice A base interface that calculates an operator table for a given operatorSet and returns its bytes representation
+/// @notice This is a base interface that all curve table calculators (eg. BN254, ECDSA) must implement
+/// @dev A single `OperatorTableCalculator` can be used for multiple operatorSets of an AVS
+interface IOperatorTableCalculator {
+    /// @notice Thrown when the operatorSet does not exist.
+    error InvalidOperatorSet();
+
+    /**
+     * @notice Sets the operatorWeightCalculator for a given operatorSet
+     * @param operatorSet The operatorSet to set the operatorWeightCalculator for
+     * @param operatorWeightCalculator The operatorWeightCalculator to set for the given operatorSet
+     * @dev This function is only callable by the owner of the contract
+     */
+    function setOperatorWeightCalculator(
+        OperatorSet calldata operatorSet,
+        IOperatorWeightCalculator operatorWeightCalculator
+    ) external;
+
+    /**
+     * @notice calculates the operatorTableBytes for a given operatorSet
+     * @param operatorSet the operatorSet to calculate the operatorTableBytes for
+     * @return operatorTableBytes The operatorTable bytes
+     */
+    function calculateOperatorTableBytes(
+        OperatorSet calldata operatorSet
+    ) external view returns (bytes memory operatorTableBytes);
+
+    /**
+     * @notice For a given operatorSet, returns the operatorWeightCalculator
+     * @param operatorSet The operatorSet to get the operatorWeightCalculator for
+     * @return operatorWeightCalculator The operatorWeightCalculator for the given operatorSet
+     * @dev This contract
+     */
+    function getOperatorWeightCalculator(
+        OperatorSet calldata operatorSet
+    ) external view returns (IOperatorWeightCalculator);
+
+    /**
+     * @notice Validates that the operatorSet exists
+     * @param operatorSet the operatorSet to validate
+     * @return true if the operatorSet exists, false otherwise
+     */
+    function validateOperatorSet(
+        OperatorSet calldata operatorSet
+    ) external view returns (bool);
+
+    /**
+     * @notice Gets the keyRegistry address
+     * @dev This contract assumes a single keyRegistry for all operatorSets
+     * @return keyRegistry the keyRegistry address
+     * @dev The keyRegistry is an immutable contract set upon deployment
+     */
+    function getKeyRegistry() external view returns (address keyRegistry);
+}
