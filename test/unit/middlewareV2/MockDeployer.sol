25d058c333ad9b82dd58656ae6583cee40befe7c
Yash Patil <40046473+ypatil12@users.noreply.github.com>
2025-05-29 16:51:49 -0400

    chore: update submodule

---
+++ test/unit/middlewareV2/MockDeployer.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.27;
+
+import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import {ITransparentUpgradeableProxy} from
+    "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+
+import {AllocationManagerMock} from "eigenlayer-contracts/src/test/mocks/AllocationManagerMock.sol";
+import {Randomness, Random} from "../../utils/Random.sol";
+
+import "forge-std/Test.sol";
+
+abstract contract MockEigenLayerDeployer is Test {
+    /// @dev addresses that should be excluded from fuzzing
+    mapping(address => bool) public isExcludedFuzzAddress;
+
+    modifier filterFuzzedAddressInputs(address addr) {
+        cheats.assume(!isExcludedFuzzAddress[addr]);
+        _;
+    }
+
+    /// @dev set the random seed for the current test
+    modifier rand(Randomness r) {
+        r.set();
+        _;
+    }
+
+    function random() internal returns (Randomness) {
+        return Randomness.wrap(Random.SEED).shuffle();
+    }
+
+    Vm cheats = Vm(VM_ADDRESS);
+
+    // State Variables
+    AllocationManagerMock public allocationManagerMock;
+
+    function _deployMockEigenLayer() internal {
+        allocationManagerMock = new AllocationManagerMock();
+    }
+}
