72b525ff4da299244185f2dd2904f02a57fbcfbb
Yash Patil <40046473+ypatil12@users.noreply.github.com>
2025-06-27 12:04:36 -0400

    chore: add interfaces

---
+++ src/interfaces/IECDSATableCalculator.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity >=0.5.0;
+
+import {OperatorSet} from "eigenlayer-contracts/src/contracts/libraries/OperatorSetLib.sol";
+import {IOperatorTableCalculator, IOperatorTableCalculatorTypes} from "eigenlayer-contracts/src/contracts/interfaces/IOperatorTableCalculator.sol";
+
+
+interface IECDSATableCalculator is
+    IOperatorTableCalculator,
+    IOperatorTableCalculatorTypes
+{
+    /**
+     * @notice calculates the operatorInfos for a given operatorSet
+     * @param operatorSet the operatorSet to calculate the operator table for
+     * @return operatorInfos the list of operatorInfos for the given operatorSet
+     * @dev The output of this function is converted to bytes via the `calculateOperatorTableBytes` function
+     */
+    function calculateOperatorTable(
+        OperatorSet calldata operatorSet
+    ) external view returns (ECDSAOperatorInfo[] memory operatorInfos);
+}
