9f1a2aa58e6bb6806eeba2c059679acdf567ce9c
Yash Patil <40046473+ypatil12@users.noreply.github.com>
2025-07-10 21:53:21 -0400

    chore: push

--- test/unit/BLSSigCheckUtilsUnit.t.sol
+++ test/unit/BLSSigCheckUtilsUnit.t.sol
@@ -9,7 +9,8 @@ contract BLSSigCheckUtilsUnitTests is Test {
     using BN254 for BN254.G1Point;
     using BLSSigCheckUtils for BN254.G1Point;
 
-    uint256 constant FP_MODULUS = 21888242871839275222246405745257275088696311157297823662689037894645226208583;
+    uint256 constant FP_MODULUS =
+        21888242871839275222246405745257275088696311157297823662689037894645226208583;
 
     struct TestPoint {
         uint256 x;
@@ -41,9 +42,9 @@ contract BLSSigCheckUtilsUnitTests is Test {
         // These are known valid points on the BN254 curve
         // We'll use the generator and its scalar multiples which are guaranteed to be on curve
         BN254.G1Point memory generator = BN254.generatorG1();
-        
+
         assertTrue(generator.isOnCurve(), "Generator should be on the curve");
-        
+
         // Test the generator negation
         BN254.G1Point memory negatedGen = generator.negate();
         assertTrue(negatedGen.isOnCurve(), "Negated generator should be on the curve");
@@ -63,7 +64,11 @@ contract BLSSigCheckUtilsUnitTests is Test {
         for (uint256 i = 0; i < invalidPoints.length; i++) {
             assertFalse(
                 invalidPoints[i].isOnCurve(),
-                string(abi.encodePacked("Invalid point ", vm.toString(i), " should not be on the curve"))
+                string(
+                    abi.encodePacked(
+                        "Invalid point ", vm.toString(i), " should not be on the curve"
+                    )
+                )
             );
         }
     }
@@ -77,15 +82,17 @@ contract BLSSigCheckUtilsUnitTests is Test {
         uint256 ySquared = mulmod(xMax, xMax, FP_MODULUS);
         ySquared = mulmod(ySquared, xMax, FP_MODULUS);
         ySquared = addmod(ySquared, 3, FP_MODULUS);
-        
+
         // This x value doesn't have a valid y on the curve, so any y should return false
         BN254.G1Point memory boundaryPoint = BN254.G1Point(xMax, 0);
-        assertFalse(boundaryPoint.isOnCurve(), "Point with x at modulus boundary should not be on curve");
+        assertFalse(
+            boundaryPoint.isOnCurve(), "Point with x at modulus boundary should not be on curve"
+        );
 
         // Test point with coordinates >= FP_MODULUS (should be reduced modulo FP_MODULUS)
         BN254.G1Point memory overflowPoint = BN254.G1Point(
             FP_MODULUS + 1, // This should be reduced to 1
-            FP_MODULUS + 2  // This should be reduced to 2
+            FP_MODULUS + 2 // This should be reduced to 2
         );
         assertTrue(overflowPoint.isOnCurve(), "Overflow point should be reduced and be on curve");
     }
@@ -95,7 +102,7 @@ contract BLSSigCheckUtilsUnitTests is Test {
      */
     function testFuzz_isOnCurve_randomPoints(uint256 x, uint256 y) public pure {
         BN254.G1Point memory point = BN254.G1Point(x, y);
-        
+
         // Calculate expected result
         uint256 xMod = x % FP_MODULUS;
         uint256 yMod = y % FP_MODULUS;
@@ -103,10 +110,10 @@ contract BLSSigCheckUtilsUnitTests is Test {
         uint256 x3 = mulmod(xMod, xMod, FP_MODULUS);
         x3 = mulmod(x3, xMod, FP_MODULUS);
         uint256 rhs = addmod(x3, 3, FP_MODULUS);
-        
+
         bool expectedOnCurve = (y2 == rhs);
         bool actualOnCurve = point.isOnCurve();
-        
+
         assertEq(actualOnCurve, expectedOnCurve, "isOnCurve result mismatch for random point");
     }
 
@@ -116,7 +123,7 @@ contract BLSSigCheckUtilsUnitTests is Test {
     function test_isOnCurve_negatedPoints() public pure {
         BN254.G1Point memory generator = BN254.generatorG1();
         BN254.G1Point memory negatedGenerator = generator.negate();
-        
+
         assertTrue(negatedGenerator.isOnCurve(), "Negated generator should be on the curve");
         assertEq(negatedGenerator.X, 1, "Negated generator X should be 1");
         assertEq(negatedGenerator.Y, FP_MODULUS - 2, "Negated generator Y should be p - 2");
@@ -127,12 +134,12 @@ contract BLSSigCheckUtilsUnitTests is Test {
      */
     function test_isOnCurve_scalarMultiplication() public view {
         BN254.G1Point memory generator = BN254.generatorG1();
-        
+
         // Test small scalar multiplications using scalar_mul_tiny
         for (uint16 i = 1; i < 10; i++) {
             BN254.G1Point memory multiplied = generator.scalar_mul_tiny(i);
             assertTrue(
-                multiplied.isOnCurve(), 
+                multiplied.isOnCurve(),
                 string(abi.encodePacked("Generator * ", vm.toString(i), " should be on the curve"))
             );
         }
@@ -144,14 +151,17 @@ contract BLSSigCheckUtilsUnitTests is Test {
      */
     function test_isOnCurve_pointAddition() public view {
         BN254.G1Point memory generator = BN254.generatorG1();
-        
+
         // Test doubling using scalar multiplication instead of addition
         // (since addition might fail without precompiles)
         BN254.G1Point memory doubled = generator.scalar_mul_tiny(2);
         assertTrue(doubled.isOnCurve(), "Doubled point should be on the curve");
-        
+
         // Test that the doubled point is different from the generator
-        assertTrue(doubled.X != generator.X || doubled.Y != generator.Y, "Doubled point should be different");
+        assertTrue(
+            doubled.X != generator.X || doubled.Y != generator.Y,
+            "Doubled point should be different"
+        );
     }
 
     /**
@@ -161,15 +171,15 @@ contract BLSSigCheckUtilsUnitTests is Test {
         // Test with very large coordinates (close to modulus)
         uint256 largeX = FP_MODULUS - 10;
         uint256 largeY = FP_MODULUS - 20;
-        
+
         BN254.G1Point memory largePoint = BN254.G1Point(largeX, largeY);
-        
+
         // Calculate expected result
         uint256 y2 = mulmod(largeY, largeY, FP_MODULUS);
         uint256 x3 = mulmod(largeX, largeX, FP_MODULUS);
         x3 = mulmod(x3, largeX, FP_MODULUS);
         uint256 rhs = addmod(x3, 3, FP_MODULUS);
-        
+
         bool expectedOnCurve = (y2 == rhs);
         assertEq(largePoint.isOnCurve(), expectedOnCurve, "Large coordinate point check failed");
     }
@@ -179,30 +189,28 @@ contract BLSSigCheckUtilsUnitTests is Test {
      */
     function test_isOnCurve_invalidXCoordinates() public pure {
         // Some x values that don't have valid y coordinates on the curve
-        uint256[5] memory invalidXs = [
-            uint256(4),
-            uint256(5),
-            uint256(7),
-            uint256(8),
-            uint256(10)
-        ];
-        
+        uint256[5] memory invalidXs = [uint256(4), uint256(5), uint256(7), uint256(8), uint256(10)];
+
         for (uint256 i = 0; i < invalidXs.length; i++) {
             // Try with y = 0 and y = 1
             BN254.G1Point memory point1 = BN254.G1Point(invalidXs[i], 0);
             BN254.G1Point memory point2 = BN254.G1Point(invalidXs[i], 1);
-            
+
             // These x values don't have valid y coordinates, so both should be false
             // (unless by chance one of these y values happens to be valid)
             uint256 x3 = mulmod(invalidXs[i], invalidXs[i], FP_MODULUS);
             x3 = mulmod(x3, invalidXs[i], FP_MODULUS);
             uint256 rhs = addmod(x3, 3, FP_MODULUS);
-            
+
             if (mulmod(0, 0, FP_MODULUS) != rhs) {
-                assertFalse(point1.isOnCurve(), "Point with invalid x and y=0 should not be on curve");
+                assertFalse(
+                    point1.isOnCurve(), "Point with invalid x and y=0 should not be on curve"
+                );
             }
             if (mulmod(1, 1, FP_MODULUS) != rhs) {
-                assertFalse(point2.isOnCurve(), "Point with invalid x and y=1 should not be on curve");
+                assertFalse(
+                    point2.isOnCurve(), "Point with invalid x and y=1 should not be on curve"
+                );
             }
         }
     }
@@ -212,14 +220,14 @@ contract BLSSigCheckUtilsUnitTests is Test {
      */
     function test_isOnCurve_gasUsage() public {
         BN254.G1Point memory generator = BN254.generatorG1();
-        
+
         uint256 gasBefore = gasleft();
         bool result = generator.isOnCurve();
         uint256 gasAfter = gasleft();
-        
+
         assertTrue(result, "Generator should be on curve");
         emit log_named_uint("Gas used for isOnCurve", gasBefore - gasAfter);
-        
+
         // Test with multiple calls to see consistency
         gasBefore = gasleft();
         for (uint256 i = 0; i < 10; i++) {
@@ -239,22 +247,24 @@ contract BLSSigCheckUtilsUnitTests is Test {
         testPoints[0].x = 1;
         testPoints[0].y = 2;
         testPoints[0].shouldBeOnCurve = true;
-        
+
         // Point with x=1, wrong y
         testPoints[1].x = 1;
         testPoints[1].y = 3;
         testPoints[1].shouldBeOnCurve = false;
-        
+
         // Another valid point
-        testPoints[2].x = 9727523064272218541460723335320998459488975639302513747055235660443850046724;
-        testPoints[2].y = 5031696974169251245229961296941447383441169981934237515842977230762345915487;
+        testPoints[2].x =
+            9727523064272218541460723335320998459488975639302513747055235660443850046724;
+        testPoints[2].y =
+            5031696974169251245229961296941447383441169981934237515842977230762345915487;
         testPoints[2].shouldBeOnCurve = true;
-        
+
         // Invalid point
         testPoints[3].x = 2;
         testPoints[3].y = 2;
         testPoints[3].shouldBeOnCurve = false;
-        
+
         for (uint256 i = 0; i < testPoints.length; i++) {
             BN254.G1Point memory point = BN254.G1Point(testPoints[i].x, testPoints[i].y);
             assertEq(
@@ -264,5 +274,4 @@ contract BLSSigCheckUtilsUnitTests is Test {
             );
         }
     }
-
-} 
+}
