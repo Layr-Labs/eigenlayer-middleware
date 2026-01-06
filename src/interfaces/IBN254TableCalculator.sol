7af7c074922ce4979508afd749477792c41f5cdb
Yash Patil <40046473+ypatil12@users.noreply.github.com>
2026-01-06 13:08:59 -0500

    fix: address gas intensive comment

--- src/interfaces/IBN254TableCalculator.sol
+++ src/interfaces/IBN254TableCalculator.sol
@@ -55,7 +55,9 @@ interface IBN254TableCalculator is IOperatorTableCalculator, IOperatorTableCalcu
      * @return nonSignerApk The aggregate BN254 G1 public key of the non-signers
      * @dev Reconstructs the operator info merkle tree deterministically to produce proofs and indices.
      * @dev The output of this function is only valid when the operator table has been freshly updated and the current operator set state exactly matches the state at
-     *      a given `referenceTimestamp`. In all other cases, the generated `nonSignerWitnesses` will be inconsistent with verification logic.
+     *      a given `referenceTimestamp`. It is recommended to call this function at blockchain state that matches the `referenceTimestamp` of the certificate being verified.
+     *      In all other cases, the generated `nonSignerWitnesses` will be inconsistent with verification logic.
+     * @dev This function is intended to be called offchain due to large gas costs.
      */
     function getNonSignerWitnessesAndApk(
         OperatorSet calldata operatorSet,
