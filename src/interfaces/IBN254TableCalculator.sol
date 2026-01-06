17105587596781e8c206f592d7003f2b4cf8dc95
Yash Patil <40046473+ypatil12@users.noreply.github.com>
2026-01-06 13:02:02 -0500

    fix: clarify getNOnSignerWitnessAndApk behavior
    chore: format

--- src/interfaces/IBN254TableCalculator.sol
+++ src/interfaces/IBN254TableCalculator.sol
@@ -54,6 +54,8 @@ interface IBN254TableCalculator is IOperatorTableCalculator, IOperatorTableCalcu
      * @return nonSignerWitnesses The witnesses for operators that did not sign
      * @return nonSignerApk The aggregate BN254 G1 public key of the non-signers
      * @dev Reconstructs the operator info merkle tree deterministically to produce proofs and indices.
+     * @dev The output of this function is only valid when the operator table has been freshly updated and the current operator set state exactly matches the state at
+     *      a given `referenceTimestamp`. In all other cases, the generated `nonSignerWitnesses` will be inconsistent with verification logic.
      */
     function getNonSignerWitnessesAndApk(
         OperatorSet calldata operatorSet,
